27ae3e0f28c42a51f55a2eccca71371e84471fc0
ftx-java <15645025191@163.com>
2020-07-14 20:19:04 +0800

    python日常练习

---
+++ day1.py
@@ -0,0 +1,15 @@
+print("Hello Python");
+print(1+1);
+a='let\'s';
+b='go';
+print(a+b);
+str='let\'s go';
+print(str);
+str1=r'let\'s go';
+print(str1);
+str2='''
+nihao
+你好
+hello
+'''
+print(str2);
